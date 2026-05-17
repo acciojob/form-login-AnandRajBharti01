3218b4c635985c57bd454c11082f2f184d6ce8e1
Anand Raj Bharti <anandrajbharti574@gmail.com>
2026-05-17 17:49:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
+	let fName = document.querySelector(input[name="fname"]);
+	let lname = document.querySelector(input[name="lname"]);
+	let btn = document.querySelector(input[name="Submit"]);
+
+	btn.addEventListenr("click", ()={
+		alert(`${fName.value} ${lName.value}`)
+	})
 
 }
